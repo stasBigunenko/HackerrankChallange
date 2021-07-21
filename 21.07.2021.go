1b76c74e16bb08a99a5dc86a75842f37c041cd98
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-07-21 14:51:58 +0300

    Update 21.07.2021.go

--- 21.07.2021.go
+++ 21.07.2021.go
@@ -1,14 +1,14 @@
 //https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
 func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
-count := int32(0)
+  count := int32(0)
 
-for i := int32(0); i < (n-1); i++ {
-for j:= i + 1; j < n; j++{
-if ( ar[i] + ar[j] ) % k == 0 {
-count++
-}
-}
-}
+  for i := int32(0); i < (n-1); i++ {
+    for j:= i + 1; j < n; j++{
+      if ( ar[i] + ar[j] ) % k == 0 {
+        count++
+      }
+    }
+  }
 return count
 }
