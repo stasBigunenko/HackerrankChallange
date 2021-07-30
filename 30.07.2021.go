24d83c2b38d426dc5023ffdb6ff658cdfdb61f04
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-07-30 09:46:41 +0300

    Update 30.07.2021.go

--- 30.07.2021.go
+++ 30.07.2021.go
@@ -1,24 +1,24 @@
 //https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
 
 func designerPdfViewer(h []int32, word string) int32 {
-		var runes []int32
-		var max, count int32
+	var runes []int32
+	var max, count int32
 
-		runes = append(runes, 'a')
+	runes = append(runes, 'a')
 
-		for i:=int32(1); i<int32(len(h)); i++ {
-			runes = append(runes, runes[0] + i)
-		}
+	for i:=int32(1); i<int32(len(h)); i++ {
+		runes = append(runes, runes[0] + i)
+	}
 
-		for i,val := range h {
-			for j := range word {
-				if byte(val) == word[j] {
-					count++
-					if max <= h[i] {
-						max = h[i]
-					}
+	for i,val := range h {
+		for j := range word {
+			if byte(val) == word[j] {
+				count++
+				if max <= h[i] {
+					max = h[i]
 				}
 			}
 		}
+	}
 	return max * count
 }
