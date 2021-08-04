2aa286da40fda555d3a34b3ace7bea81e29763ff
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-08-04 09:04:04 +0300

    Add files via upload

---
+++ 04.08.2021.go
@@ -0,0 +1,21 @@
+//https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+func equalizeArray(arr []int32) int32 {
+	var num, sum1, sum2 int32
+	sum2 = 0
+
+	for i:=0; i<len(arr); i++ {
+		num = arr[i]
+		sum1 = 1
+		for j:=i+1; j<len(arr); j++ {
+			if num == arr[j] {
+				sum1++
+			}
+		}
+		if sum2 < sum1 {
+			sum2 = sum1
+		}
+	}
+	qty := int32(len(arr)) - sum2
+	return qty
+}
