0afec88459dc394197047863df8fdd0d02ed042a
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-08-07 12:20:42 +0300

    Update 07.08.2021.go

--- 07.08.2021.go
+++ 07.08.2021.go
@@ -1,8 +1,7 @@
 //https://www.hackerrank.com/challenges/chocolate-feast/problem
 
 func chocolateFeast(n int32, c int32, m int32) int32 {
-    	var sum int32 = 0
-    	sum = n / c
+        sum := n / c
     
       	for i:=sum; i>=m; i-=m {
        	 	sum++
