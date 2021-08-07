dcbd04c1001324fea20a5182b136f706c629ebd3
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-08-07 12:19:42 +0300

    Update 07.08.2021.go

--- 07.08.2021.go
+++ 07.08.2021.go
@@ -3,10 +3,10 @@
 func chocolateFeast(n int32, c int32, m int32) int32 {
     	var sum int32 = 0
     	sum = n / c
-   	wrap := sum
-  	for i:=wrap; i>=m; i-=m {
+    
+      	for i:=sum; i>=m; i-=m {
        	 	sum++
         	i++
-   	}
+    	}
     	return sum
 }
