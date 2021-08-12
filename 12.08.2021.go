6bab2bf48181f8193eb9782e3b5afb7c00fb71fa
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-08-12 10:34:54 +0300

    Add files via upload

---
+++ 12.08.2021.go
@@ -0,0 +1,36 @@
+//https://www.hackerrank.com/challenges/encryption/problem
+
+//needed additional import math to find square root
+
+func encryption(s string) string {
+	var row, col int
+	var res string
+	var s1 []string
+
+	sqr := math.Sqrt(float64(len(s)))
+	intpart, decpart := math.Modf(sqr)
+	if decpart != 0 {
+		row = int(sqr)
+		col = int(sqr+1)
+	}else {
+		row = int(intpart)
+		col = int(intpart)
+	}
+	if col * row < len(s) {
+		row++
+	}
+
+	s1 = strings.Split(s, "")
+
+	for i:=0; i<col; i++ {
+		k := i
+		for j:=0; j<row; j++ {
+			if k < len(s) {
+				res += s1[k]
+				k += col
+			}
+		}
+		res += " "
+	}
+	return res
+}
