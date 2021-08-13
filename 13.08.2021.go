147243dc312ab66cc2bd87e7b69341a74b06eb89
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-08-13 10:30:51 +0300

    Add files via upload

---
+++ 13.08.2021.go
@@ -0,0 +1,25 @@
+//https://www.hackerrank.com/challenges/repeated-string/problem
+
+func repeatedString(s string, n int64) int64 {
+	count := int64(0)
+
+	for _, val := range s {
+		if val == 'a' {
+			count++
+		}
+	}
+
+	p := n / int64(len(s))
+	count = count * p
+
+	k := n % int64(len(s))
+	if k != 0 {
+		for i:=0;i<int(k); i++ {
+			if s[i] == 'a' {
+				count++
+			}
+		}
+	}
+	return count
+}
+	}
