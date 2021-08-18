571df9f6d099b0df7ffa9c8073aa8ed45b890517
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-08-18 10:11:37 +0300

    Update 18.08.2021.go

--- 18.08.2021.go
+++ 18.08.2021.go
@@ -1,19 +1,15 @@
 //https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
 
 func jumpingOnClouds(c []int32) int32 {
-	var count int32 = -1
+    var count int32 = -1
 
-	for i:=0; i<len(c); i++{
-		count++
-		if (i+2 < len(c)) {
-			if c[i+2] == int32(0) {
-				i += 1
-				continue
-			} else if c[i+2] == c[i+1] {
-				i += 1
-				continue
-			}
-		}
-	}
-	return count
+    for i:=0; i<len(c); i++{
+        count++
+        if (i+2 < len(c)) {
+            if c[i+2] == int32(0) || c[i+2] == c[i+1] {
+                i += 1
+            }
+        }
+    }
+    return count
 }
