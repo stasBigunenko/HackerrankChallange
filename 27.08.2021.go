04525b08792389a03c908a673cacb6727e79ec6c
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-08-27 09:37:58 +0300

    Add files via upload

---
+++ 27.08.2021.go
@@ -0,0 +1,20 @@
+//https://www.hackerrank.com/challenges/pangrams/problem
+
+func pangrams(s string) string {
+	count:= [26]int{}
+	var res string = "pangram"
+
+	str := strings.ToLower(s)
+
+	for _, val := range str {
+		if val >= 'a' && val <= 'z' {
+			count[val-'a'] = 1
+		}
+	}
+	for i:=0; i < 26; i++ {
+		if count[i] != 1 {
+			res = "not pangram"
+		}
+	}
+	return res
+}
