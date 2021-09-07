1bf3585e93a9f47a1545d4c028f2e1f565a2984f
stasBigunenko <87713771+stasBigunenko@users.noreply.github.com>
2021-09-07 10:35:58 +0300

    Add files via upload

---
+++ 07.09.2021.go
@@ -0,0 +1,18 @@
+//https://www.hackerrank.com/challenges/game-of-thrones/problem
+
+func twoStrings(s1 string, s2 string) string {
+	str := [26]int{0}
+	res := "NO"
+
+	for i := 0; i < len(s1); i++ {
+		str[s1[i]-'a']++
+		}
+
+	for i := 0; i < len(s2); i++ {
+		if str[s2[i]-'a'] > 0 {
+			res = "YES"
+			break
+		}
+	}
+	return res
+}
